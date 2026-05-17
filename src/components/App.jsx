51bd4b17ecff7b9085c556f15a720b522f356fa9
Nasra <nasra.hassan@student.moringaschool.com>
2026-05-17 18:47:29 +0300

    Implement full CRUD functionality for Toy Tales app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,65 @@
-import React, { useState } from "react";
-
-import Header from "./Header";
-import ToyForm from "./ToyForm";
+import { useEffect, useState } from "react";
 import ToyContainer from "./ToyContainer";
+import ToyForm from "./ToyForm";
 
 function App() {
+  const [toys, setToys] = useState([]);
   const [showForm, setShowForm] = useState(false);
 
-  function handleClick() {
-    setShowForm((showForm) => !showForm);
+  // GET request
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((r) => r.json())
+      .then((data) => setToys(data));
+  }, []);
+
+  // POST request
+  function addToy(newToy) {
+    setToys([...toys, newToy]);
+  }
+
+  // DELETE request
+  function deleteToy(id) {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      const updatedToys = toys.filter((toy) => toy.id !== id);
+      setToys(updatedToys);
+    });
+  }
+
+  // PATCH request
+  function likeToy(updatedToy) {
+    const updatedToys = toys.map((toy) =>
+      toy.id === updatedToy.id ? updatedToy : toy
+    );
+
+    setToys(updatedToys);
   }
 
   return (
-    <>
-      <Header />
-      {showForm ? <ToyForm /> : null}
-      <div className="buttonContainer">
-        <button onClick={handleClick}>Add a Toy</button>
+    <div>
+      <div id="toy-header">
+        <img
+          src="https://fontmeme.com/permalink/180719/67429e6afec53d21d64643101c43f029.png"
+          alt="toy header"
+        />
       </div>
-      <ToyContainer />
-    </>
+
+      {showForm ? <ToyForm addToy={addToy} /> : null}
+
+      <div className="buttonContainer">
+        <button onClick={() => setShowForm(!showForm)}>
+          Add a Toy
+        </button>
+      </div>
+
+      <ToyContainer
+        toys={toys}
+        deleteToy={deleteToy}
+        likeToy={likeToy}
+      />
+    </div>
   );
 }
 
--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+function ToyCard({ toy, deleteToy, likeToy }) {
 
-function ToyCard() {
+  function handleLike() {
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        likes: toy.likes + 1,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedToy) => likeToy(updatedToy));
+  }
+
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
-      <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
-        className="toy-avatar"
-      />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <img src={toy.image} alt={toy.name} className="toy-avatar" />
+
+      <h2>{toy.name}</h2>
+
+      <p>{toy.likes} Likes </p>
+
+      <button onClick={handleLike}>
+        Like {"<3"}
+      </button>
+
+      <button onClick={() => deleteToy(toy.id)}>
+  Donate to GoodWill
+</button>
     </div>
   );
 }
